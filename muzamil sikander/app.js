2528cc120afcbb1b0d57555c0ce605228a380f79
muzamilsikander <102538533+Muzamil-Sikander@users.noreply.github.com>
2022-06-21 21:42:34 +0500

    Add files via upload

---
+++ muzamil sikander/app.js
@@ -0,0 +1,55 @@
+// var positiveInt = parseInt(prompt("please Enter your value !"));
+
+// document.write( "number: "  + positiveInt + "<br>");
+// document.write( "roundoff value: "  + Math.round(positiveInt) + "<br>");
+// document.write( "Floor value :"  + Math.floor(positiveInt) + "<br>");
+// document.write( "ceil number: "  + Math.ceil(positiveInt) + "<br>");
+
+
+
+// Question # 2
+
+// var negativeInt = parseInt(prompt("please Enter your value !"));
+
+// document.write( "number: "  + negativeInt + "<br>");
+// document.write( "roundoff value: "  + Math.round(negativeInt) + "<br>");
+// document.write( "Floor value :"  + Math.floor(negativeInt) + "<br>");
+// document.write( "ceil number: "  + Math.ceil(negativeInt) + "<br>");
+
+
+
+// question # 3
+
+
+// var positiveFloat = parseFloat(prompt("please Enter your value !"));
+
+// document.write( "number: "  + positiveFloat + "<br>");
+// document.write( "roundoff value: "  + Math.round(positiveFloat) + "<br>");
+// document.write( "Floor value :"  + Math.floor(positiveFloat) + "<br>");
+// document.write( "ceil number: "  + Math.ceil(positiveFloat) + "<br>");
+
+// question # 4
+// var negativeFloat = parseFloat(prompt("please Enter your value !"));
+
+// document.write( "number: "  + negativeFloat + "<br>");
+// document.write( "roundoff value: "  + Math.round(negativeFloat) + "<br>");
+// document.write( "Floor value :"  + Math.floor(negativeFloat) + "<br>");
+// document.write( "ceil number: "  + Math.ceil(negativeFloat) + "<br>");
+
+
+// question # 5
+
+// var absvalue = parseInt(prompt("please Enter your value !"));
+// document.write(" the absolute value  of " + absvalue  +  " is "  + Math.abs(absvalue) );
+
+// question # 6
+
+// var randomvalue = [ "1","2", "3","4" ,"5", "6", "8" ,"9" ]  ;
+// document.write("Random dice value" + Math.round(Math.random(randomvalue)*10) + "<br>" )
+// document.write("Random dice value" + Math.round(Math.random(randomvalue)*10) + "<br>" )
+
+// questiom # 7
+
+// var num5 = ["head", "Tail"]
+// document.write(  Math.round(Math.random()*1 ) + "<br>" + "Random coin value " + num5[Math.round(Math.random(num5)*1)] + "<br>")
+// document.write(  Math.round(Math.random()*1 ) + "<br>" + "Random coin value " + num5[Math.round(Math.random(num5)*1)])
